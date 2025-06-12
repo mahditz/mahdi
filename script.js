6181936671e52486208830dbe4f042384435dc84
Mahdi TZ <97670594+mahditz@users.noreply.github.com>
2025-06-12 05:40:39 -0700

    Add Persian cosmetics store template

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+    document.querySelectorAll('.fade-in').forEach(el => {
+        const rect = el.getBoundingClientRect();
+        if (rect.top <= window.innerHeight - 100) {
+            el.classList.add('show');
+        }
+    });
+});
